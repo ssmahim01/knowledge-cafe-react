4946ffcf5311386ad1760f52a234e331a3067d3d
ssmahim01 <mahimzayn666@gmail.com>
2024-10-21 22:17:20 +0600

    Initial project setup have done and Added crucial files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-center text-4xl font-bold'>Knowledge Cafe React</h1>
+        </div>
+    );
+};
+
+export default Header;
